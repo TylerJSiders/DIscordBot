dcb058d490a42bd27d2ffd065bd991a5674f9ee4
TylerJSiders <78033735+TylerJSiders@users.noreply.github.com>
2021-12-09 10:46:08 -0500

    bot pulls joke every hour

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const discord = require('discord.io');
 const tokenConstant = require('./token.constant');
 const cron = require("cron").CronJob;
+const axios = require("axios").default;
 
 var bot = new discord.Client({
     autorun: true,
     token: tokenConstant.AUTH_TOKEN
 });
 
-
-
-
-
 bot.on('ready', () => {
     console.log("Bot is ready");
-})
 
+});
 
-
-var FiveMinuteJoke = new cron(
-    '0 0 * * * * ',
+var HourlyJoke = new cron(
+    '0 0 7-23 * * * ',
     function () {
-        bot.sendMessage({
-            to: "918329606644334624",
-            message: "Test Message",
+        axios.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist&type=twopart").then((result) => {
+            var CHANNELID = "918348732121100318";
+
+            bot.sendMessage({
+                to: CHANNELID,
+                message: "Setup: " + result.data.setup
+            });
+
+            bot.sendMessage({
+                to: CHANNELID,
+                message: "Punchline: " + result.data.delivery,
+            });
         });
     },
     null,
