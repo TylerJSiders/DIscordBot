6620f4289867171983fb232f3d503b63eb98adeb
Tyler Siders <78033735+TylerJSiders@users.noreply.github.com>
2022-01-17 17:40:27 -0500

    Works

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+const https = require('https');
 const discord = require('discord.io');
 const tokenConstant = require('./token.constant');
 const cron = require("cron").CronJob;
-const axios = require("axios").default;
+
+const axios = require("axios").default.create({
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+    })
+});
+
+const baseUrl = 'https://localhost:7140';
 
 var bot = new discord.Client({
     autorun: true,
     token: tokenConstant.AUTH_TOKEN
 });
 
-bot.on('ready', () => {
-    console.log("Bot is ready");
 
+//Timed and Event Triggers
+bot.on('ready', () => {
+    console.log("Bot is Ready");
 });
+
+bot.on('message', (userName, userId, channelId, message) => {
+    axios.get(`${baseUrl}/api/users/${userId}/exists`).then((response) => {
+        if (!response.data) {
+            createUser(userId, userName);
+        }
+    });
+});
+
 
 var HourlyJoke = new cron(
     '0 0 7-23 * * * ',
@@ -34,3 +52,18 @@ var HourlyJoke = new cron(
     true,
     "America/New_York"
 );
+
+//Functions
+function createUser(userId, userName) {
+    let userModel = {
+        id: userId,
+        userName: userName
+    };
+
+    axios.post(`${baseUrl}/api/users`, userModel).then((result) => {
+        //Do Nothing at the moment
+        //TODO add a welcome message for first posters
+    }, () => {
+        console.log("An Error has happened.");
+    });
+}
